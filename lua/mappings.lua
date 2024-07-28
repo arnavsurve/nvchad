af1f2f9b35fbc782a09bce5ee2fbe7acedace8df
Arnav Surve <arnavsurve@gmail.com>
2024-07-28 14:41:28 -0700

    initial commit

---
+++ lua/mappings.lua
@@ -0,0 +1,9 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", "<leader>e", "<cmd> NvimTreeToggle <cr>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
